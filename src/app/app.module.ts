763d704823940185b33177b1a6696c6f2b139565
Antonio <antonioramoshernandez1997@hotmail.com>
2022-05-11 15:55:52 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgChartsModule } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraComponent } from './components/barra/barra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraComponent
   ],
   imports: [
     BrowserModule,
+    NgChartsModule,
     AppRoutingModule
   ],
   providers: [],
